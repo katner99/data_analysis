bd2ce42d5d1d74ee4cab19e2498e1d12c0dc6b32
Kat Turner <kturner@tiscali.it>
2023-02-01 13:41:55 +0000

    created a comparison between interdecadal timeseries

--- PI_processing/PI_timeseries.py
+++ PI_processing/PI_timeseries.py
@@ -6,15 +6,24 @@ import numpy as np
 import sys
 import datetime
 import netCDF4 as nc
-from plots import make_interannual_timeseries
+from plots import make_interannual_timeseries, compare_timeseries_TSM
 import numpy as np
 
 
 if __name__ == "__main__":
-    filepath = "/data/oceans_output/shelf/katner33/PIctrl_output/PAS_ctrl07/output/"
+    filepath_ens07 = "/data/oceans_output/shelf/katner33/PIctrl_output/PAS_ctrl07/output/"
+    filepath_ens08 = "/data/oceans_output/shelf/katner33/PIctrl_output/PAS_ctrl08/output/"
     filename = "output.nc"
     start_year = 2070
     n_years = 29
     var = "THETA"
-    make_interannual_timeseries(filepath, filename, start_year, n_years, var)
+    [theta07,time]=make_interannual_timeseries(filepath_ens07, filename, start_year, n_years, var, plot = False, save = False, show = False)
+    [theta08,time]=make_interannual_timeseries(filepath_ens08, filename, start_year, n_years, var, plot = False, save = False, show = False)
+    var = "SALT"
+    [salt07,time]=make_interannual_timeseries(filepath_ens07, filename, start_year, n_years, var, plot = False, save = False, show = False)
+    [salt08,time]=make_interannual_timeseries(filepath_ens08, filename, start_year, n_years, var, plot = False, save = False, show = False)
+    var = "SIfwmelt"
+    [melt07,time]=make_interannual_timeseries(filepath_ens07, filename, start_year, n_years, var, plot = False, save = False, show = False)
+    [melt08,time]=make_interannual_timeseries(filepath_ens08, filename, start_year, n_years, var, plot = False, save = False, show = False)
 
+    compare_timeseries_TSM(time, theta07, theta08, salt07, salt08, melt07, melt08, "ens_07", "ens08")
