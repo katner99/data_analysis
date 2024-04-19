40e54d3df369027610265e190f614af7b006f133
Kat Turner <kturner@tiscali.it>
2024-04-19 14:27:04 +0100

    adjusted the scripts for the comparison plot

--- PI_processing/config_options.py
+++ PI_processing/config_options.py
@@ -26,7 +26,7 @@ Variables:
 import xarray as xr
 import numpy as np
 from mitgcm_python.grid import Grid
-from funcs import find_nearest
+from funcs import find_nearest, read_variable
 from directories_and_paths import grid_filepath
 
 grid = Grid(grid_filepath)
@@ -141,3 +141,105 @@ def config_timeseries(var, data, experiments, ensemble):
         }
 
     return plot_info
+
+
+def config_comparison(var, input_data, grid, period):
+    """
+    Configures plotting parameters and data for a comparison plot of various oceanographic variables.
+
+    Parameters:
+    - var (str): Variable to be plotted (e.g., "THETA", "SIheff", "SALT", "SHIfwFlx").
+    - input_data (list): List of input datasets.
+    - grid (object): Grid object for the model grid.
+    - period (str): Time period for the comparison plot.
+
+    Returns:
+    - data (list): List of data arrays to be plotted.
+    - graph_params (dict): Dictionary containing parameters for the main plot.
+    - graph_params_anom (dict): Dictionary containing parameters for the anomaly plot.
+    """
+    # temperature
+    if var == "THETA":
+        depth_range = [
+            find_nearest(input_data[0]["Z"].values, -200),
+            find_nearest(input_data[0]["Z"].values, -700),
+        ]
+        data = [read_variable(input, var, grid, depth_range) for input in input_data]
+        color_scheme = "coolwarm"
+        color_sceme_anom = color_scheme
+        anom = 1.5
+        min_val = -2
+        max_val = 2.1
+        title = f"Average temperature between 200 and 700m {period}"
+        interval = 1
+        interval_anom = 0.5
+    # sea ice thickness
+    elif var == "SIheff":
+        data = [read_variable(input, var, grid) for input in input_data]
+        color_scheme = "jet"
+        anom = 1
+        min_val = 0
+        max_val = 2
+        interval = 0.25
+        interval_anom = 0.25
+        title = f"Sea ice thickness (m) {period}"
+        color_sceme_anom = "PRGn_r"
+    # sea ice tickness
+    elif var in ["SIheff", "oceFWflx", "SIfwmelt", "SIfwfrz", "EXFvwind", "oceQnet"]:
+        data = [
+            read_variable(input, var, grid) * 3600 * 24 * 365 / 1000
+            for input in input_data
+        ]
+        color_scheme = "PRGn_r"
+        anom = 1.5
+        min_val = -5
+        max_val = 5
+        title = f"Freshwater fluxes m/yr {period}"
+        color_sceme_anom = "PRGn_r"
+
+    # salinity
+    elif var == "SALT":
+        data = [read_variable(input, var, grid) for input in input_data]
+        color_scheme = "PRGn_r"
+        anom = 0.25
+        min_val = int(np.min(data[0]))
+        max_val = int(np.max(data[0])) + 1
+        title = f"Surface Salinity {period}"
+        interval = 1
+        interval_anom = 0.25
+        color_sceme_anom = "PRGn_r"
+
+    elif var == "SHIfwFlx":
+        data = [
+            -read_variable(input, var, grid) * 3600 * 24 * 30 * 10 ** (-3)
+            for input in input_data
+        ]
+        color_scheme = "rainbow"
+        color_sceme_anom = "PRGn_r"
+        anom = 25
+        min_val = 0
+        max_val = 76
+        title = f"ice shelf basal melt rate ({period}) (m.w.e./yr)"
+        interval = 20
+        interval_anom = 10
+
+    # graph parameters 1.333112e-05 -1.0214189e-06
+    graph_params = {
+        "font_size": 12,
+        "low_val": min_val,
+        "high_val": max_val,
+        "interval": interval,
+        "color_scheme": color_scheme,
+        "step": 20,
+        "title": title,
+    }
+
+    graph_params_anom = {
+        "font_size": 12,
+        "low_val": -anom,
+        "high_val": anom,
+        "interval": interval_anom,
+        "color_scheme": color_sceme_anom,
+        "step": 20,
+    }
+    return data, graph_params, graph_params_anom
---
+++ PI_processing/contour_comparison.py
@@ -0,0 +1,66 @@
+from config_options import config_comparison
+from plots import read_mask
+from plots_2d import comparison
+from directories_and_paths import output_path, grid_filepath
+from mitgcm_python.grid import Grid
+
+import sys
+import xarray as xr
+
+
+def main():
+    # set up the variables you need
+    var = "THETA"
+    save = True
+    show = True
+    period = "2070-2100"
+
+    # load up the file paths for the monster
+    filepaths = [
+        output_path + "average_" + ens + "_" + period + ".nc"
+        for ens in ["CTRL", "LENS", "WIND", "TEMP"]
+    ]
+    # check if the input files exist
+    for filepath in filepaths:
+        try:
+            open(filepath)
+        except FileNotFoundError:
+            sys.exit(f"Stopped - Could not find input file {filepath}")
+
+    experiment = ["pre-industrial", "RCP 8.5", "wind f.", "thermo. f."]
+
+    # load up the input data
+    input_data = [
+        xr.open_dataset(filepath, decode_times=False) for filepath in filepaths
+    ]
+
+    # read in the general variables (these should be the same between the ensembles
+    set_up = read_mask(input_data[0])
+
+    grid = Grid(grid_filepath)
+
+    [data, graph_params, graph_params_anom] = config_comparison(
+        var, input_data, grid, period
+    )
+
+    file_out = "mega_comparison" + var + "_" + period + ".png"
+
+    residual = data[0] + data[1] - data[2] - data[3]
+
+    comparison(
+        data,
+        set_up,
+        graph_params,
+        graph_params_anom,
+        experiment,
+        graph_params["title"],
+        file_out,
+        save,
+        show,
+        True,
+        residual,
+    )
+
+
+if __name__ == "__main__":
+    main()  # run the program
