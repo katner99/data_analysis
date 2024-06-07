9e18a2f88d4d619af101f3c357dba850ec1639e7
Kat Turner <kturner@tiscali.it>
2024-06-07 11:13:09 +0100

    working on testing the averages and the significance of differences in the ensembles

--- PI_processing/stats.py
+++ PI_processing/stats.py
@@ -1,88 +1,79 @@
 import numpy as np
 from scipy import stats
-from plots import plot_timeseries_comparison
-from directories_and_paths import *
-from config_options import *
-import matplotlib
-matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
+from directories_and_paths import output_path, lens_path
 
-def calc_pvalue(data, experiments, ensemble):
-    """
-    The calc_pvalue function efficiently computes p-values for statistical comparison between 
-    two specified experiments using ensemble data. It iterates over time intervals, extracting 
-    temperature values for each experiment and ensemble member to perform independent t-tests. 
-    The resulting p-values are collected and returned as a comprehensive list for further analysis 
-    or visualization.
-    Parameters:
-        data: Dictionary containing experimental data.
-        experiments: List containing identifiers for the two experiments to compare.
-        ensemble: List of ensemble members.
-    Returns:
-        p_value: A list of p-values corresponding to each time interval, indicating the significance 
-        of differences between the specified experiments.
-    """
-    p_value = []
-    
-    for time in range(181*12):
-        values_1 = np.array([data[experiments[0]][ens][0] for ens in ensemble])[:, time]
-        values_2 = np.array([data[experiments[1]][ens][0] for ens in ensemble])[:, time]
-        
-        test = stats.ttest_ind(values_1, values_2)
-        p_value.append(test.pvalue)
-    return p_value
-    
-def stat_timeseries(var, data, ensemble, experiments, experiment_names, file_out = None):
-    """
-    Parameters:
-        data: Dictionary containing experimental data.
-        ensemble: List of ensemble members.
-        experiments: List containing two experiment identifiers for comparison.
-        experiment_names: List of full experiment names.
-        file_out (optional): Output file path to save generated plots.
-    """
-    
-    p_value = calc_pvalue(data, experiments, ensemble)
-    
-    for time in range(181*12):
-        values_1 = np.array([data[experiments[0]][ens][0] for ens in ensemble])[:, time]
-        values_2 = np.array([data[experiments[1]][ens][0] for ens in ensemble])[:, time]
-        
-        test = stats.ttest_ind(values_1, values_2)
-        p_value.append(test.pvalue)
-    
-    plot_info = {
-        "var": var,
-        "data": data,
-        "experiments": experiments,
-        "ensemble_members": ensemble,
-        "ylabel": "Temperature (degC)",
-        "time": 181*12,
-        "xlabel": np.arange(1921, 2100, 10),
-        "x_lim": [12, (2099-1920)*12],
-        "smooth": 2,
-        "shade_range": True,
-        "experiment_full": experiment_names,
-    }
-    
-    fig, axs = plt.subplots(2, 1, figsize=(10, 8), sharex=True)
-    
-    # Plot p-value against time
-    axs[0].plot(p_value, color='black')
-    axs[0].set_xlim([12, 180*12])
-    axs[0].set_ylabel('P-value', fontsize=14)
-    axs[0].set_title(f"P-value {experiments[0]} vs. {experiments[1]}", fontsize=14)
-    axs[0].axhline(0.05, color='red')
+import xarray as xr
 
-    # Plot time series comparison
-    plot_timeseries_comparison(axs[1], var, data, experiments, ensemble, plot_info)
+def calc_average(experiments, var):
+    ensemble = [1,2,3,4,5,6,7,8,9]
+    years = range(2070, 2100)
 
-    plt.tight_layout()
-    plt.show()
+    expy_vals = []
+    for exp in experiments:
+        ensemble_data = []
+        for ens in ensemble:
+            time_mean = [] 
+            for year in years:
+                filepath = f"output/{year}01/MITgcm/output.nc"
+                if exp == "LENS":
+                    if ens < 6:
+                        filepath = f"{lens_path}PAS_LENS00{ens}_noOBC/{filepath}"
+                    else:
+                        filepath = f"{output_path}PAS_LENS00{ens}_noOBC/{filepath}"
+                else:
+                    filepath = f"{output_path}{exp}_ens0{ens}_noOBC/{filepath}"
+
+                try:
+                    with xr.open_dataset(filepath) as data_member:
+                        input_data = data_member[var].where(data_member.hFacC == 1).mean(dim = "time")
+                        subset = input_data.sel(Z = slice(-200, -700)).mean(dim = "Z")                        
+                        time_mean.append(subset)
+                
+                except Exception as e:
+                    print(f"Error reading file: {filepath}, {e}")
+
+            ensemble_data.append(xr.concat(time_mean, dim = "year").mean(dim = "year"))
+            print(filepath)
+
+        expy_vals.append(xr.concat(ensemble_data, dim = "ensemble")) 
     
-    if file_out:
-        fig.savefig(file_out)
-    else:
-        fig.savefig(f"pvalue_{experiments[0]}vs{experiments[1]}.png")
+    expy_vals = xr.concat(expy_vals, dim = "experiment")
+    print(expy_vals)
+    return expy_vals
+
+def main():
+    experiments = ["CTRL", "WIND"]
+    expy_vals = calc_average(experiments, "THETA")
+
+    filename = f"{experiments[1]}_pvalue.nc"
+    # save incase the next bit dies RIP
+    expy_vals.to_netcdf(filename)
+
+    theta_vals = expy_vals.THETA.values
+    pval = np.empty(theta_vals[0,0,...].shape)
+
+    for lat in range(len(expy_vals.YC.values)):
+        for lon in range(len(expy_vals.XC.values)):
+            values_1 = theta_vals[0, :, lat, lon]
+            values_2 = theta_vals[1, :, lat, lon]
+            test = stats.ttest_ind(values_1, values_2)
+            pval[lat, lon] = test.pvalue
+
+
+    trend = xr.Dataset(
+        {
+            'pvalue': (['lat', 'lon'], pval)
+        },
+        coords={'lat':expy_vals.YC.values, 'lon': expy_vals.XC.values}
+    )
+
+    filename = f"{experiments[1]}_pvalue.nc"
+    # save incase the next bit dies RIP
+    trend.to_netcdf(filename)
+    
+
+if __name__ == "__main__":
+    main()
+
     
 
