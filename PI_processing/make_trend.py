289218b2d69422de7240b28c0a346c82396f2fe0
Kat Turner <kturner@tiscali.it>
2024-06-07 11:12:32 +0100

    still working on getting parallelisation to work on the slices

--- PI_processing/make_trend.py
+++ PI_processing/make_trend.py
@@ -3,8 +3,9 @@ import xarray as xr
 import numpy as np
 import pandas as pd
 from scipy.stats import linregress
-from directories_and_paths import output_path, lens_path
+from directories_and_paths import output_path, lens_path, grid_filepath
 from multiprocessing import Pool, cpu_count
+from mitgcm_python.grid import Grid
 
 def concatenate_var_years(args):
     """ reads and concatentates years """
@@ -20,7 +21,7 @@ def concatenate_var_years(args):
         filepath = f"{output_path}{experiment}_ens0{ens_member}_noOBC/"
     
     filepaths = [f"{filepath}output/{year}01{filename}" for year in range(1920, 2101)]
-    
+
     total_data = []
     for file in filepaths:
         data = xr.open_dataset(file, decode_times=False)
@@ -28,82 +29,62 @@ def concatenate_var_years(args):
             total_data.append(data[var])
         elif option == "slice":
             from config_options import lat_slices, lon_slices
-            sliced_data = data[var].sel(XC = lon_slices, method = "nearest")
+            sliced_data = data[var].sel(XG = lon_slices, method = "nearest")
             sliced_data = sliced_data.sel(YC = slice(lat_slices[0], lat_slices[1]))
             total_data.append(sliced_data)
-    
+       
     total_dataset = xr.concat(total_data, dim='time')
+    
     time_coords = pd.date_range(start="1920-01-01", periods=len(total_dataset), freq="M")
     total_dataset['time'] = time_coords
-    
+
     return total_dataset
 
-def total_trend(filename, var, experiment):
-    """calculates trend for simple 2d variable with dims time, lat, lon"""
-    dataset = xr.open_dataset(filename, decode_times=False)
+    if option == 'total':
+        reference_date = np.datetime64('1920-01-01')
+        time = time_coords - reference_date
+        time = time / np.timedelta64(1, 'Y')
+
+        slopes, r_values, p_values = calculate_trend_total(total_dataset, time)
         
-    lat = dataset.YC.values
-    lon = dataset.XC.values
-    time = dataset.time.values
-    data = dataset[var].values
+        lat = data.YC.values
+        lon = data.XC.values
 
-    slopes, r_values, p_values = calculate_trend(data, time)
+        trend_ds = xr.Dataset(
+            {
+                'trend': (['lat', 'lon'], slopes),
+                'pvalue': (['lat', 'lon'], p_values),
+                'rvalue': (['lat', 'lon'], r_values)
+            },
+            coords={'lat': lat, 'lon': lon}
+        )
 
-    trend_ds = xr.Dataset(
-        {
-            'trend': (['lat', 'lon'], slopes),
-            'pvalue': (['lat', 'lon'], p_values),
-            'rvalue': (['lat', 'lon'], r_values)
-        },
-        coords={'lat': lat, 'lon': lon}
-    )
+    elif option == "slice":
+        reference_date = np.datetime64('1920-01-01')
+        time = time_coords - reference_date
+        time = time / np.timedelta64(1, 'Y')
 
-    trend_ds.to_netcdf(f"{output_path}{experiment}_files_temp/{var}_trend.nc")
-
-def slice_trend(filename, var, experiment):
-    """ calculates trend over specific slices of the ocean, dims time, depth, lat, lon """
-    dataset = xr.open_dataset(filename, decode_times=False)
+        slopes, r_values, p_values = calculate_trend_slice(sliced_data, time)
         
-    lat = dataset.YC.values
-    lon = dataset.XC.values
-    depth = dataset.Z.values
-    time = dataset.time.values
-    data = dataset[var].values
+        z = data.Z.values
+        lat = data.YC.values
+        if var == "UVEL":
+            lon = data.XG.values
+        else:
+            lon = data.XC.values
 
-    def calculate_trend_slice(z):
-        slopes = np.empty(len(lat))
-        r_values = np.empty(len(lat))
-        p_values = np.empty(len(lat))
-        for y in range(len(lat)):
-            for x in lon:
-                slope, intercept, r_value, p_value, std_err = linregress(time, data[:,z, y, x])
-                slopes[y] = slope
-                r_values[y] = r_value
-                p_values[y] = p_value
-        
-        return slopes, r_values, p_values
+        trend_ds = xr.Dataset(
+            {
+                'trend': (['z', 'lat', 'lon'], slopes),
+                'pvalue': (['z', 'lat', 'lon'], p_values),
+                'rvalue': (['z', 'lat', 'lon'], r_values)
+            },
+            coords={'z': z, 'lat': lat, 'lon': lon}
+        )
 
-    num_processes = min(cpu_count(), 22)
+    return trend_ds
 
-    with Pool(processes=num_processes) as pool:
-        results = pool.map(calculate_trend_slice, range(len(depth)))
-
-    slopes = np.stack([result[0] for result in results])
-    r_values = np.stack([result[1] for result in results])
-    p_values = np.stack([result[2] for result in results])
-
-    trend_ds = xr.Dataset(
-        {
-            'trend': (['depth', 'lat', 'lon'], slopes),
-            'pvalue': (['depth', 'lat', 'lon'], p_values),
-            'rvalue': (['depth', 'lat', 'lon'], r_values)
-        },
-        coords={'depth': depth, 'lat': lat, 'lon': lon}
-    )
-
-    trend_ds.to_netcdf(f"{output_path}{experiment}_files_temp/{var}_trend.nc")
-
-def calculate_trend(data, time):
+def calculate_trend_total(data, time):
     """calculates trend for the slices (attempt at parallelisation)"""
     slopes = np.empty(data.shape[1:])
     r_values = np.empty(data.shape[1:])
@@ -117,6 +98,37 @@ def calculate_trend(data, time):
             p_values[y, x] = p_value
 
     return slopes, r_values, p_values
+
+def calculate_trend_slice(data, time):
+    """calculates trend for the slices (attempt at parallelisation)"""
+    slopes = np.empty(data.shape[1:])
+    r_values = np.empty(data.shape[1:])
+    p_values = np.empty(data.shape[1:])
+
+    for z in range(data.shape[1]):
+        for y in range(data.shape[2]):
+            for x in range(data.shape[3]):
+                slope, intercept, r_value, p_value, std_err = linregress(time, data[:, z, y, x])
+                slopes[z, y, x] = slope
+                r_values[z, y, x] = r_value
+                p_values[z, y, x] = p_value
+
+    return slopes, r_values, p_values
+
+def calculate_trend_profile(data, time):
+    """calculates trend for the slices (attempt at parallelisation)"""
+    slopes = np.empty(data.shape[1:])
+    r_values = np.empty(data.shape[1:])
+    p_values = np.empty(data.shape[1:])
+
+    for z in range(data.shape[1]):
+        slope, intercept, r_value, p_value, std_err = linregress(time, data[:, z])
+        slopes[z] = slope
+        r_values[z] = r_value
+        p_values[z] = p_value
+
+    return slopes, r_values, p_values
+
 
 def main():
     """ 
@@ -136,24 +148,81 @@ def main():
 
     args_list = [(experiment, var, option, str(ens_member)) for ens_member in ensemble_members]
     
-    num_processes = min(22, cpu_count())
+    #num_processes = min(22, cpu_count())
     
     # I want to calculate trend with time so need all the years to be concatenated
-    with Pool(processes=num_processes) as pool:
-        results = pool.map(concatenate_var_years, args_list)
-    
-    ensemble_mean = xr.concat(results, dim='ensemble_member').mean(dim='ensemble_member')
-    filename = f"{output_path}{experiment}_files_temp/{var}.nc"
+    results = map(concatenate_var_years, args_list)
+
+    ensemble_spread = xr.concat(results, dim='ensemble_member').mean(dim='ensemble_member')
+
+    print(np.shape(ensemble_spread))
+
+    if option == "profile":
+        from config_options import slice_ranges
+        lon_range = slice_ranges["lon_range_cont"]
+        lat_range = slice_ranges["lat_range_cont"]
+        sliced_data = ensemble_spread.sel(XC = slice(lon_range[0], lon_range[1]), YC = slice(lat_range[0], lat_range[1]))
+        time_coords = ensemble_spread.time
+        profile_dataset = sliced_data.where(sliced_data.hFacC == 1).mean(dim=["XC", "YC"])
+        profile_dataset.to_netcdf(f"output_path{experiment}_temp_files/{var}_profile.nc")
+        
+        reference_date = np.datetime64('1920-01-01')
+        time = time_coords - reference_date
+        time = time / np.timedelta64(1, 'Y')
+
+        slopes, r_values, p_values = calculate_trend_profile(profile_dataset, time)
+        
+        z = sliced_data.Z.values
+
+        trend_ds = xr.Dataset(
+            {
+                'trend': (['z'], slopes),
+                'pvalue': (['z'], p_values),
+                'rvalue': (['z'], r_values)
+            },
+            coords={'z': z}
+        )
+
+    filename = f"{output_path}{experiment}_files_temp/{var}_spread.nc"
     # save incase the next bit dies RIP
-    ensemble_mean.to_netcdf(filename)
+    ensemble_spread.to_netcdf(filename)
 
-    filename = f"{output_path}{experiment}_files_temp/{var}.nc"
+def calc_trend():
+    experiment = str(sys.argv[1]) 
+    var = "SALT"
+    filename = f"{output_path}{experiment}_files_temp/{var}_spread.nc"
+    dataset = xr.open_dataset(filename, decode_times=False)
 
-    # calculate trend
-    if option == "total":
-        total_trend(filename, var, experiment)
-    else:
-        slice_trend(filename, var, experiment)
+    data = dataset[var].values
+    time = dataset.time.values
+    lat = dataset.YC.values
+    lon = dataset.XC.values
+    depth = dataset.Z.values
+
+    slopes = np.empty(data.shape[1:])
+    r_values = np.empty(data.shape[1:])
+    p_values = np.empty(data.shape[1:])
+
+    for z in range(data.shape[1]):
+        for y in range(data.shape[2]):
+            for x in range(data.shape[3]):
+                slope, intercept, r_value, p_value, std_err = linregress(time, data[:, z, y, x])
+                slopes[z, y, x] = slope
+                r_values[z, y, x] = r_value
+                p_values[z, y, x] = p_value
+
+    trend_ds = xr.Dataset(
+        {
+            'trend': (['z', 'lat', 'lon'], slopes),
+            'pvalue': (['z', 'lat', 'lon'], p_values),
+            'rvalue': (['z', 'lat', 'lon'], r_values)
+        },
+        coords={'z': depth, 'lat': lat, 'lon': lon}
+    )
+
+    filename = f"{output_path}{experiment}_files_temp/{var}_trend.nc"
+    # save incase the next bit dies RIP
+    trend_ds.to_netcdf(filename)
 
 if __name__ == "__main__":
-    main()
+    calc_trend()
