c623f5133838fc6970d7d14251a15f2fc9fa9cfa
Kat Turner <kturner@tiscali.it>
2024-08-21 15:37:36 +0100

    refactored the script to make compliant with the rest

--- PI_processing/contour_comparison.py
+++ PI_processing/contour_comparison.py
@@ -1,81 +1,101 @@
+"""
+Plotting script for generating comparison plots of oceanographic data.
+"""
+
 from config_options import config_comparison
-from plots import read_mask
-from plots_2d import comparison
-from directories_and_paths import output_path, grid_filepath
-from mitgcm_python.grid import Grid
+from tools.plots import read_mask
+from tools.plots_2d import comparison
+from directories_and_paths import output_path
 
 import sys
 import xarray as xr
 
-def read_data(var, var_name = None):
-    """ read in analysed data """
-    if var in ["trend", "pvalue"]:
-        filename = f"{var_name}_spread.nc"
-        file_out = f"mega_comparison_{var}_{var_name}.png"
-    elif var == "mean":
-        filename = f"{var_name}.nc"
-        file_out = f"mega_comparison_{var}_{var_name}_mean.png"
-    filepaths = [
-        f"{output_path}{exp}_files_temp/{filename}"
-        for exp in ["CTRL", "LENS", "WIND", "TEMP"]
-    ]
+def read_data(var, var_name=None):
+    """
+    Reads in analyzed data based on the variable type.
+
+    Parameters:
+    - var (str): Type of data to read ("trend", "pvalue", "mean").
+    - var_name (str): Name of the variable (default is None).
+
+    Returns:
+    - filepaths (list): List of file paths to the data files.
+    - file_out (str): Output filename for the plot.
+    """
+    file_prefix = f"{output_path}{{}}_files_temp/{{}}"
+    filename = f"{var_name}_spread.nc" if var in ["trend", "pvalue"] else f"{var_name}.nc"
+    file_out = f"mega_comparison_{var}_{var_name}.png" if var in ["trend", "pvalue"] else f"mega_comparison_{var}_{var_name}_mean.png"
+    
+    experiments = ["CTRL", "LENS", "WIND", "TEMP"]
+    filepaths = [file_prefix.format(exp, filename) for exp in experiments]
+    
+    return filepaths, file_out
+
+def read_climatology_data(period):
+    """
+    Generates file paths and output filename for climatology data.
+
+    Parameters:
+    - period (str): Time period for climatology data.
+
+    Returns:
+    - filepaths (list): List of file paths to the data files.
+    - file_out (str): Output filename for the plot.
+    """
+    experiments = ["CTRL", "LENS", "WIND", "TEMP"]
+    filepaths = [f"{output_path}average_{exp}_{period}.nc" for exp in experiments]
+    file_out = f"mega_comparison_climatology_{period}.png"
+    
     return filepaths, file_out
 
 def main():
-    """ plot function to create the ugly 4x4 beast of a plot """
-    var = "mean"             # either set to the parameter you will be using or set to mean or trend (tells it where to read the values in)
-    save = True              # save figure
-    show = False             # print the figure on script completion
+    """
+    Main function to create the 4x4 comparison plot.
+    """
+    var = "SIheff"  
+    save = True     # Save figure to file
+    show = False    # Display figure after script completion
+    loc = "trend"   # Set to the parameter or "mean" or "trend" to determine data source
 
-    # will follow the first option if going through analysed data, will otherwise get the climatology data for the corresponding var
-    if var in ["trend", "pvalue", "mean"]:
-        var_name = "oceFWflx"
-        [filepaths, file_out] = read_data(var, var_name)
+    if loc in ["trend", "mean"]:
+        filepaths, file_out = read_data(loc, var)
     else:
         period = "2070-2100"
-        filepaths = [
-            f"{output_path}average_{ens}_{period}.nc"
-            for ens in ["CTRL", "LENS", "WIND", "TEMP"]
-        ]
-        file_out = f"mega_comparison{var}_{period}.png"
+        filepaths, file_out = read_climatology_data(period)
 
+    # Check for missing files
     for filepath in filepaths:
-        try:
-            open(filepath)
-        except FileNotFoundError:
+        if not open(filepath, 'r'):
             sys.exit(f"Stopped - Could not find input file {filepath}")
 
-    # experiment neames to add to the plot
-    experiment = ["NONE", "ALL", "WIND", "THERMO"]
+    # Experiment names for the plot
+    experiments = ["NONE", "ALL", "WIND", "THERMO"]
 
-    input_data = [
-        xr.open_dataset(filepath, decode_times=False) for filepath in filepaths
-    ]
+    # Load datasets
+    input_data = [xr.open_dataset(filepath, decode_times=False) for filepath in filepaths]
+   
+    # Set up data for plotting
+    set_up = read_mask()
 
-    grid = Grid(grid_filepath)
-
-    # set up the data to be plotted
-    if var in ["trend", "pvalue", "mean"]:
-        set_up_data = xr.open_dataset(f"{output_path}average_CTRL_1920-1950.nc", decode_times=False)
-        set_up = read_mask(set_up_data)
-        [data, graph_params, graph_params_anom] = config_comparison(var, input_data, grid, var_name = var_name)
+    # Configure plot parameters
+    if loc in ["trend", "pvalue", "mean"]:
+        data, graph_params, graph_params_anom = config_comparison(var, input_data, loc=loc)
     else:
-        set_up = read_mask(input_data[0])
-        [data, graph_params, graph_params_anom] = config_comparison(var, input_data, grid, period = period)
+        data, graph_params, graph_params_anom = config_comparison(var, input_data, period=period)
 
-    # plot the data
+    # Generate the comparison plot
     comparison(
         data,
         set_up,
         graph_params,
         graph_params_anom,
-        experiment,
+        experiments,
         file_out,
         save,
         show,
-        False,       # option to calculate the linearity in the plot
-        "cont_shelf" # option to zoom into a certain area, choose between "cont_shelf" and "ice_shelf" or leave blank
+        linearity=False,   # Option to calculate linearity in the plot
+        zoom="cont_shelf"  # Option to zoom into a specific area: "cont_shelf", "ice_shelf", or leave blank
     )
 
 if __name__ == "__main__":
-    main() 
+    main()
